6631f6d0edd0bd257e605dc6be897f78c49359b7
paddypopeye <paddypopeye@hotmail.com>
2017-11-08 15:09:07 +0100

    Lateset Update

--- jchat/jchat/admin.py
+++
@@ -1,9 +0,0 @@
-# -*- encoding: UTF-8 -*-
-"""
-@author: Federico Cáceres <fede.caceres@gmail.com>
-"""
-from django.contrib import admin
-from chat.models import *
-
-admin.site.register(Room)
-admin.site.register(Message)
